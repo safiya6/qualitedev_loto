bcf2b7cdcc66be90f668abe57f787f42f147a0f1
safiya <safiya.azouagh@edu-univ-paris13.fr>
2024-10-12 16:25:17 +0200

    front

---
+++ Views/view_begin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Loto - 10x Plus de Chances</title>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&family=Raleway:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="Content/style.css">
+    </head>
