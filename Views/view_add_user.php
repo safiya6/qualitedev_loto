2a1a01ee1fad31207456ff25d220d6ceec3437e8
safiya <safiya.azouagh@edu-univ-paris13.fr>
2024-10-13 09:40:22 +0200

    changement bouton supprier

--- Views/view_add_user.php
+++ Views/view_add_user.php
@@ -16,7 +16,12 @@
                     <!-- Formulaire pour le bouton de suppression -->
                     <form action="?controller=joueurs&action=deleteUser" method="POST" class="delete-form">
                         <input type="hidden" name="id_joueur" value="<?= $joueur['id_joueur'] ?>">
-                        <button type="submit" class="delete-button">🗑️</button>
+                        <button type="submit" class="delete-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5v-8zM4.118 4a1 1 0 0 1 .82-.4h6.144a1 1 0 0 1 .82.4l.845 1H3.273l.845-1zM1 4.5A.5.5 0 0 1 1.5 4h13a.5.5 0 0 1 .5.5V5h-15v-.5zM2 5.5v9A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-9H2z"/>
+                            </svg>
+                        </button>
+
                     </form>
                 </div>
             <?php endforeach; ?>
