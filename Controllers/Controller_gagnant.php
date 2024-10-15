ac88a98f4213b7afa1eaa181421d93574faeff88
safiya <safiya.azouagh@edu-univ-paris13.fr>
2024-10-15 07:23:19 +0200

    trier ticket

---
+++ Controllers/Controller_gagnant.php
@@ -0,0 +1,41 @@
+<?php
+class Controller_game extends Controller
+{
+    /**
+     * Fonction qui trie les numéros et les étoiles d'un ticket dans l'ordre décroissant.
+     */
+    private function sortTicket($ticket)
+    {
+        // Séparer les numéros et les étoiles
+        $numbers = explode("-", explode(" | ", $ticket)[0]);
+        $stars = explode("-", explode(" | ", $ticket)[1]);
+
+        // Convertir en entiers, puis trier en ordre décroissant
+        $numbers = array_map('intval', $numbers);
+        $stars = array_map('intval', $stars);
+        rsort($numbers);
+        rsort($stars);
+
+        // Retourner le ticket trié
+        return [
+            'numbers' => $numbers,
+            'stars' => $stars
+        ];
+    }
+
+    public function action_default()
+    {
+        // Exemple d'utilisation
+        $userTicket = "12-3-25-18-7 | 9-2"; // Exemple de ticket utilisateur
+        $winningTicket = "5-15-9-30-1 | 8-3"; // Exemple de ticket gagnant
+        
+        // Trier les tickets
+        $sortedUserTicket = $this->sortTicket($userTicket);
+        $sortedWinningTicket = $this->sortTicket($winningTicket);
+        
+        // Affichage des tickets triés pour vérification
+        var_dump($sortedUserTicket);
+        var_dump($sortedWinningTicket);
+    }
+}
+?>
