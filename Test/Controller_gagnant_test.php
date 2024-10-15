1634a56fa2f4d2d6ded85b177e89ce3ed335f175
safiya <safiya.azouagh@edu-univ-paris13.fr>
2024-10-15 19:00:07 +0200

    Test

---
+++ Test/Controller_gagnant_test.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ControllerGagnantTest extends TestCase
+{
+    public function testCalculateScoreWithEqualNumbersAndStars()
+    {
+        $controller = new Controller_gagnant();
+        $userTicket = "5-15-9-30-1 | 8-3";
+        $winningTicket = "5-15-9-30-1 | 8-3";
+
+        $result = $controller->calculateScore($userTicket, $winningTicket);
+
+        $this->assertEquals(5, $result['numero_egalite']);
+        $this->assertEquals(2, $result['etoile_egalite']);
+        $this->assertEquals(0, $result['ecart']);
+    }
+
+    public function testCalculateScoreWithPartialMatch()
+    {
+        $controller = new Controller_gagnant();
+        $userTicket = "5-15-8-25-1 | 8-3";
+        $winningTicket = "5-15-9-30-1 | 8-2";
+
+        $result = $controller->calculateScore($userTicket, $winningTicket);
+
+        $this->assertEquals(3, $result['numero_egalite']);
+        $this->assertEquals(1, $result['etoile_egalite']);
+        $this->assertGreaterThan(0, $result['ecart']);
+    }
+}
