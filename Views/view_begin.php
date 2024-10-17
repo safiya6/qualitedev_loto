60a60e09710b8fe1db4cdecb70c4c6943e805e06
safiya <safiya.azouagh@edu-univ-paris13.fr>
2024-10-17 10:10:27 +0200

    ajout boutton vue _tirage

--- Views/view_begin.php
+++ Views/view_begin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Site</title>
+    <title>Loto</title>
     <link rel="stylesheet" href="Content/style.css">
 </head>
 <body>
--- Views/view_tirage.php
+++ Views/view_tirage.php
@@ -9,7 +9,7 @@
         <h3>Ticket Gagnant</h3>
         <p><?= htmlspecialchars($_SESSION['winningTicket']) ?></p>
     </div>
-
+    <a class="button-tirage" href="?controller=partie">Refaire une simulation</a>
     <!-- Section des gagnants du tirage -->
     <div class="winner-list">
         <h3>Liste des Gagnants</h3>
@@ -58,50 +58,5 @@
         margin: 0;
     }
 
-    .container-winning {
-        width: 100%;
-        max-width: 800px;
-        margin-top: 20px;
-        background-color: #ffffff;
-        padding: 20px;
-        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
-        border-radius: 10px;
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-        align-items: center;
-    }
-
-    .page-title {
-        text-align: center;
-        font-size: 2em;
-        color: #333;
-    }
-
-    /* Ticket gagnant */
-    .winning-ticket {
-        text-align: center;
-        padding: 15px;
-        background-color: #e8f5e9;
-        border: 1px solid #c8e6c9;
-        border-radius: 8px;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-        font-size: 1.2em;
-        color: #388e3c;
-        font-weight: bold;
-        width: 100%;
-    }
-
-    /* Table des gagnants */
-    .winner-list {
-        text-align: center;
-        width: 100%;
-    }
-
-    .winner-list h3 {
-        color: #444;
-        font-size: 1.5em;
-        margin-bottom: 15px;
-    }
-
+    
     .
